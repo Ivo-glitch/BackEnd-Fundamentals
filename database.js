4cf65e9601e3b7eb728606095d02b1f445141907
Ivan Sotelo <soteloivanmanuel26@gmail.com>
2022-02-15 17:36:11 -0300

    creando function insert

---
+++ database.js
@@ -0,0 +1,33 @@
+const mysql = require ('mysql2')
+
+//CONEXIÓN
+const connection = mysql.createConnection({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password: 'root',
+    database:'proyectoBackEnd1'
+})
+
+//FUNCION QUERY
+function query ( sql, data ){
+    return new Promise ((resolve,reject)=>{
+        connection.query( sql, data, function(e,result){
+            if(e){
+                reject(e.sqlMessage)
+            }else{
+                result(resolve)
+            }
+        })
+    })
+}
+
+//Funcion INSERT
+async function insert(tableName, data){
+    try{
+        await query(`INSERT INTO ${tableName}(??) VALUES(?)`,[Object.keys(data), Object.values(data)])
+        return {data, success:true}
+    }catch(e){
+        return {e,success:false}
+    }
+}
